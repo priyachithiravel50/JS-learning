db813d6e50f9cfce678f60dd364cda6ec7355bf9
priyachithiravel50 <priyachithiravel50@gmail.com>
2024-10-14 16:04:49 +0530

    Today Updates

---
+++ main.js
@@ -0,0 +1,87 @@
+
+// Arithmetic Operators
+let a = 10;
+let b = 5;
+
+// Addition
+let add = a + b; 
+console.log("add:", add);
+
+// Subtraction
+let subtract = a - b; 
+console.log("subtract:", subtract);
+
+// Multiplication
+let Product = a * b; 
+console.log("Product:", Product);
+
+// Division
+let divide = a / b; 
+console.log("divide:", divide);
+
+// Modulus
+let Modulus = a % b; 
+console.log("Modulus:", Modulus);
+
+// Exponentiation
+let Power = a ** b; 
+console.log("Power:", Power);
+
+//increment
+var s = 5;
+
+// Pre increment
+var preincrement = ++s; 
+console.log("value of preincrement:", preincrement);
+
+// Post increment
+var l = 8;
+var postincrement = l++; 
+console.log("value of Postincrement:", postincrement); 
+console.log("New value of l:", l);
+
+
+//decrement
+var s = 10;
+
+// Pre decrement
+var predecrement = --s; 
+console.log("value of Predecrement:", predecrement);
+
+// Post decrement
+var l = 6;
+var postdecrement = l--; 
+console.log("value of Postdecrement:", postdecrement); 
+console.log("New value of l:", l);
+
+
+//if statement
+var age = 18;
+
+if (age >= 18) {
+    console.log("This is a book");
+}
+
+//if else statement
+var age = 16;
+
+if (age >= 18) {
+    console.log("This is a book");
+} 
+else {
+    console.log("This is not a book");
+}
+
+//if,elseif,else statement
+var score = 85;
+
+if (score >= 90) {
+    console.log("I am working in the office");
+} 
+else if (score >= 80) {
+    console.log("I am working in the house");
+} 
+else {
+    console.log("I am not work");
+}
+
