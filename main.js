a48cfb064f4cc165e701a20407b4c45028c2ff94
priyachithiravel50 <priyachithiravel50@gmail.com>
2024-10-17 16:04:31 +0530

    Today Updates

--- main.js
+++ main.js
@@ -1,194 +1,233 @@
-// function example() {
-//     var x = 10;  
-//     if (true) {
-//         let y = 20; 
-//         const z = 30;
-//         console.log(x, y, z); 
-//     }
-//     console.log(x); 
-//     console.log(y);
-//     console.log(z); 
+// // function example() {
+// //     var x = 10;  
+// //     if (true) {
+// //         let y = 20; 
+// //         const z = 30;
+// //         console.log(x, y, z); 
+// //     }
+// //     console.log(x); 
+// //     console.log(y);
+// //     console.log(z); 
+// // }
+// // example();
+
+// // Arithmetic Operators
+// let a = 10;
+// let b = 5;
+
+// // Addition
+// let add = a + b; 
+// console.log("add:", add);
+
+// // Subtraction
+// let subtract = a - b; 
+// console.log("subtract:", subtract);
+
+// // Multiplication
+// let Product = a * b; 
+// console.log("Product:", Product);
+
+// // Division
+// let divide = a / b; 
+// console.log("divide:", divide);
+
+// // Modulus
+// let Modulus = a % b; 
+// console.log("Modulus:", Modulus);
+
+// // Exponentiation
+// let Power = a ** b; 
+// console.log("Power:", Power);
+
+// //increment
+// var s = 5;
+
+// // Pre increment
+// var preincrement = ++s; 
+// console.log("value of preincrement:", preincrement);
+
+// // Post increment
+// var l = 8;
+// var postincrement = l++; 
+// console.log("value of Postincrement:", postincrement); 
+// console.log("New value of l:", l);
+
+
+// //decrement
+// var s = 10;
+
+// // Pre decrement
+// var predecrement = --s; 
+// console.log("value of Predecrement:", predecrement);
+
+// // Post decrement
+// var l = 6;
+// var postdecrement = l--; 
+// console.log("value of Postdecrement:", postdecrement); 
+// console.log("New value of l:", l);
+
+// //Assignment operators
+// let x;
+
+// // Basic Assignment
+// x = 10;
+// console.log("x =", x); 
+
+// // Addition Assignment
+// x += 5; x=x+5
+// console.log("x += 5:", x); 
+
+// // Subtraction Assignment
+// x -= 3;
+// console.log("x -= 3:", x);
+
+// //Multiplication Assignment
+// x *=2
+// console.log("x *= 2:", x);
+
+// // Division Assignment
+// x /= 4;
+// console.log("x /= 4:", x); 
+
+// // Remainder Assignment
+// x %= 2; 
+// console.log("x %= 2:", x); 
+
+// // Exponentiation Assignment
+// x = 2; 
+// x **= 3; 
+// console.log("x **= 3:", x); 
+
+// //comparison operators
+// //  (==)
+// console.log(5 == '5'); 
+
+// // (!=)
+// console.log(5 != '5');
+
+// // (===)
+// console.log(5 === '5'); 
+
+// // (!==)
+// console.log(5 !== '5'); 
+
+// // (>)
+// console.log(7 > 5); 
+
+// //  (>=)
+// console.log(5 >= 5); 
+
+// // (<)
+// console.log(3 < 5); 
+
+// // (<=)
+// console.log(5 <= 5); 
+
+
+// //logical operator
+
+// let d = 5, f = 10;
+
+// // AND (&&)
+// console.log(d > 10 && f > 0); 
+
+// // OR (||)
+// console.log(d > 0 || f < 0); 
+
+// // NOT (!) 
+// console.log(!(d > f)); 
+
+// //Data types
+
+// let number = 42;                     
+// console.log("Number:", number);     
+
+// let text = "Hello";                  
+// console.log("String:", text);       
+
+// let star = true;                   
+// console.log("Boolean:", star);     
+
+// let value;                 
+// console.log("Undefined:", value); 
+
+// let values = null;             
+// console.log("Null:", values);    
+
+// let word = Symbol('id');        
+// console.log("Symbol:", word);    
+
+// let Number = 9007089999997655n;   
+// console.log("BigInt:", Number);    
+
+
+
+// //if statement
+// var age = 18;
+
+// if (age >= 18) {
+//     console.log("This is a book");
 // }
-// example();
 
-// Arithmetic Operators
-let a = 10;
-let b = 5;
+// //if else statement
+// var age = 16;
 
-// Addition
-let add = a + b; 
-console.log("add:", add);
+// if (age >= 18) {
+//     console.log("This is a book");
+// } 
+// else {
+//     console.log("This is not a book");
+// }
 
-// Subtraction
-let subtract = a - b; 
-console.log("subtract:", subtract);
+// //if,elseif,else statement
+// var score = 85;
 
-// Multiplication
-let Product = a * b; 
-console.log("Product:", Product);
-
-// Division
-let divide = a / b; 
-console.log("divide:", divide);
-
-// Modulus
-let Modulus = a % b; 
-console.log("Modulus:", Modulus);
-
-// Exponentiation
-let Power = a ** b; 
-console.log("Power:", Power);
-
-//increment
-var s = 5;
-
-// Pre increment
-var preincrement = ++s; 
-console.log("value of preincrement:", preincrement);
-
-// Post increment
-var l = 8;
-var postincrement = l++; 
-console.log("value of Postincrement:", postincrement); 
-console.log("New value of l:", l);
-
-
-//decrement
-var s = 10;
-
-// Pre decrement
-var predecrement = --s; 
-console.log("value of Predecrement:", predecrement);
-
-// Post decrement
-var l = 6;
-var postdecrement = l--; 
-console.log("value of Postdecrement:", postdecrement); 
-console.log("New value of l:", l);
-
-//Assignment operators
-let x;
-
-// Basic Assignment
-x = 10;
-console.log("x =", x); 
-
-// Addition Assignment
-x += 5; x=x+5
-console.log("x += 5:", x); 
-
-// Subtraction Assignment
-x -= 3;
-console.log("x -= 3:", x);
-
-//Multiplication Assignment
-x *=2
-console.log("x *= 2:", x);
-
-// Division Assignment
-x /= 4;
-console.log("x /= 4:", x); 
-
-// Remainder Assignment
-x %= 2; 
-console.log("x %= 2:", x); 
-
-// Exponentiation Assignment
-x = 2; 
-x **= 3; 
-console.log("x **= 3:", x); 
-
-//comparison operators
-//  (==)
-console.log(5 == '5'); 
-
-// (!=)
-console.log(5 != '5');
-
-// (===)
-console.log(5 === '5'); 
-
-// (!==)
-console.log(5 !== '5'); 
-
-// (>)
-console.log(7 > 5); 
-
-//  (>=)
-console.log(5 >= 5); 
-
-// (<)
-console.log(3 < 5); 
-
-// (<=)
-console.log(5 <= 5); 
-
-
-//logical operator
-
-let d = 5, f = 10;
-
-// AND (&&)
-console.log(d > 10 && f > 0); 
-
-// OR (||)
-console.log(d > 0 || f < 0); 
-
-// NOT (!) 
-console.log(!(d > f)); 
-
-//Data types
-
-let number = 42;                     
-console.log("Number:", number);     
-
-let text = "Hello";                  
-console.log("String:", text);       
-
-let star = true;                   
-console.log("Boolean:", star);     
-
-let value;                 
-console.log("Undefined:", value); 
-
-let values = null;             
-console.log("Null:", values);    
-
-let word = Symbol('id');        
-console.log("Symbol:", word);    
-
-let Number = 9007089999997655n;   
-console.log("BigInt:", Number);    
+// if (score >= 90) {
+//     console.log("I am working in the office");
+// } 
+// else if (score >= 80) {
+//     console.log("I am working in the house");
+// } 
+// else {
+//     console.log("I am not work");
+// }
 
 
 
-//if statement
-var age = 18;
+//Switch Statement
+const day = "Thursday";
 
-if (age >= 18) {
-    console.log("This is a book");
+switch (day) {
+  case "Monday":
+    console.log("Start of the week");
+    break;
+  case "Tuesday":
+    console.log("It's Tuesday");
+    break;
+  case "Wednesday":
+    console.log("Midweek day");
+    break;
+  case "Thursday":
+    console.log("It's Thursday");
+    break;
+  case "Friday":
+    console.log("last day");
+    break;
+  case "Saturday":
+  case "Sunday":
+    console.log("End of the week");
+    break;
+  default:
+    console.log("Invalid day");
 }
 
-//if else statement
-var age = 16;
 
-if (age >= 18) {
-    console.log("This is a book");
-} 
-else {
-    console.log("This is not a book");
+
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  console.log("Username:", username);
+  console.log("Password:", password);
 }
 
-//if,elseif,else statement
-var score = 85;
-
-if (score >= 90) {
-    console.log("I am working in the office");
-} 
-else if (score >= 80) {
-    console.log("I am working in the house");
-} 
-else {
-    console.log("I am not work");
-}
 
